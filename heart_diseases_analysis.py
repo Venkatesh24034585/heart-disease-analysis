175ae029bf6f64b3bf9118b99ed63c63b16344e9
Venkatesh24034585 <venkateshthorlikonda53@gmail.com>
2024-11-04 11:53:53 +0300

    initial commit: add imports for modules

---
+++ heart_diseases_analysis.py
@@ -0,0 +1,13 @@
+# Import required libraries
+!pip install pandas numpy seaborn matplotlib scikit-learn
+
+# Import the libraries
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import warnings
+warnings.filterwarnings('ignore')
